c7ce20ff5f12c715aa112f2ac0ca68568e8a4e33
MikeDevUcamp <115725749+MikeDevUcamp@users.noreply.github.com>
2023-03-13 19:11:18 -0600

    Initial Commit

---
+++ proyIntermedio/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  proyIntermedio
+//
+//  Created by Miguel Gómez Díaz on 13/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
